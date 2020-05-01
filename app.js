7cc6c1ab8f36598f042e95effce12495c1103996
julie.thul <djthul@yahoo.com>
2020-04-30 21:58:50 -0500

    Slow down speed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var exerciseSpeed = 1000; //time between exercise images
+var exerciseSpeed = 800; //time between exercise images
 var exerciseLength = 10000; //total exercise time
-var wordPause = 2500; //pause between changing words
+var wordPause = 2000; //pause between changing words
 var wordList = ['am','and','at','are','can','come','go','for','like','got','me','here','see','not','the','play','to','said ','is','you','dad','day','he','down','in','into','it','looking','look','she','mom','they','my','went','on','where','up','will','we','your','I','out','a','what','has','this','that','want'] // e.g. ['the','look','good']
 var song = new Audio('./sounds/HappyPlace.mp3')
 
@@ -11,7 +11,7 @@ var start = async () => {
   var startButton = document.getElementById("startButton");
   startButton.classList.add("hidden");
 
-  await showWords(2, wordPause)
+  await showWords(6, wordPause)
   cycle('pushup1.png','pushup2.png', exerciseSpeed)
   await wait(exerciseLength)
 
@@ -23,6 +23,9 @@ var start = async () => {
   cycle('lunge1.png','lunge2.png', exerciseSpeed)
   await wait(exerciseLength)
 
+  await showWords(6, wordPause)
+  cycle('squat1.png','squat2.png', exerciseSpeed)
+  await wait(exerciseLength)
   reset()
   document.getElementById("root").addEventListener("click", pause);
 }
