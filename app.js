9db52c52ee3dd8e735e2a78c323673c5c3b7195e
Dan Thul <djthul@gmail.com>
2020-05-01 09:27:42 -0500

    Pause functionality and aspect ratio on images

--- app.js
+++ app.js
@@ -3,36 +3,33 @@ var exerciseLength = 10000; //total exercise time
 var wordPause = 2000; //pause between changing words
 var wordList = ['am','and','at','are','can','come','go','for','like','got','me','here','see','not','the','play','to','said ','is','you','dad','day','he','down','in','into','it','looking','look','she','mom','they','my','went','on','where','up','will','we','your','I','out','a','what','has','this','that','want'] // e.g. ['the','look','good']
 var song = new Audio('./sounds/HappyPlace.mp3')
+var volume = 0.5 // 0.5 = 50%
 
 var start = async () => {
-  song.volume = 0.5
-
+  song.volume = volume
+  paused = false
   song.play()
   var startButton = document.getElementById("startButton");
   startButton.classList.add("hidden");
+  document.getElementById("root").addEventListener("click", pause);
 
   await showWords(6, wordPause)
-  cycle('pushup1.png','pushup2.png', exerciseSpeed)
-  await wait(exerciseLength)
+  await cycle('pushup1.png','pushup2.png', exerciseSpeed, exerciseLength)
 
   await showWords(6, wordPause)
-  cycle('jumpingjack1.png','jumpingjack2.png', exerciseSpeed)
-  await wait(exerciseLength)
+  await cycle('jumpingjack1.png','jumpingjack2.png', exerciseSpeed, exerciseLength)
   
   await showWords(6, wordPause)
-  cycle('lunge1.png','lunge2.png', exerciseSpeed)
-  await wait(exerciseLength)
+  await cycle('lunge1.png','lunge2.png', exerciseSpeed, exerciseLength)
 
   await showWords(6, wordPause)
-  cycle('squat1.png','squat2.png', exerciseSpeed)
-  await wait(exerciseLength)
+  await cycle('squat1.png','squat2.png', exerciseSpeed, exerciseLength)
 
   await showWords(6, wordPause)
-  cycle('crunch1.png','crunch2.png', exerciseSpeed)
-  await wait(exerciseLength)
+  await cycle('crunch1.png','crunch2.png', exerciseSpeed, exerciseLength)
 
   reset()
-  document.getElementById("root").addEventListener("click", pause);
+
 }
 
 
--- helpers.js
+++ helpers.js
@@ -19,15 +19,14 @@ function shuffle(array) {
 
 var currentWord = 0; //always 0 don't touch
 var currentSlide = 1; //always 1 don't touch
-var interval = null;  //always null don't touch
 var timeout = null;
+var paused = true;
 var reset = () => {
-  if (interval) clearInterval(interval)
   if (timeout) clearTimeout(timeout)
   currentWord = 0; //always 0 don't touch
   currentSlide = 1; //always 1 don't touch
-  interval = null;  //always null don't touch
   var timeout = null;
+  paused = true;
   shuffle(wordList)
   if (song) {
     song.pause()
@@ -37,50 +36,52 @@ var reset = () => {
 }
 
 var pause = () => {
+  paused = true
   if (song) {
     song.pause()
   }
-  if (interval) clearInterval(interval)
+  if (timeout) timeout = null
   startButton.classList.remove("hidden");
   document.getElementById("root").addEventListener("click", start);
 }
 
-var cycle = (img1, img2, pause) =>  {
+var cycle = async (img1, img2, pause, cycleLength) =>  {
+  var numOfSlides = (cycleLength/pause).toFixed(0)
   var src = document.getElementById("root")
   var img = document.createElement("img");
   img.src = `./images/${img1}`;
   src.appendChild(img);
 
-  currentSlide = 2
-  interval = setInterval(() => {
-    var src = document.getElementById("root")
-    src.innerHTML = '';
-    var img = document.createElement("img");
-    if (currentSlide === 1) {
-      img.src = `./images/${img1}`;
-      currentSlide = 2;
-    } else {
-      img.src = `./images/${img2}`;
-      currentSlide = 1
+  currentSlide = 1
+  for (let index = 0; index < numOfSlides; index++) {
+    if (!paused) {
+      var src = document.getElementById("root")
+      src.innerHTML = '';
+      var img = document.createElement("img");
+      if (currentSlide === 1) {
+        img.src = `./images/${img1}`;
+        currentSlide = 2;
+      } else {
+        img.src = `./images/${img2}`;
+        currentSlide = 1
+      }
+  
+      src.appendChild(img);
+      timeout = await new Promise(resolve => setTimeout(resolve, pause))
     }
-
-    src.appendChild(img);
-  }, pause)
-}
-
-var wait = async pauseTime => {
-  timeout = await new Promise(resolve => setTimeout(resolve, pauseTime))
-  clearInterval(interval)
+  }
 }
 
 var showWords = async (numWords, wordPause) => {
   for (let index = 0; index < numWords; index++) {
-    document.getElementById("root").innerHTML = `<span class='sightWord'>${wordList[currentWord]}</span>`;
-    await new Promise(resolve => setTimeout(resolve, wordPause))
-    if (currentWord < wordList.length - 1) {
-      currentWord++
-    } else {
-      currentWord = 0
+    if (!paused) {
+      document.getElementById("root").innerHTML = `<span class='sightWord'>${wordList[currentWord]}</span>`;
+      timeout = await new Promise(resolve => setTimeout(resolve, wordPause))
+      if (currentWord < wordList.length - 1) {
+        currentWord++
+      } else {
+        currentWord = 0
+      }
     }
   }
   document.getElementById("root").innerHTML = '';
